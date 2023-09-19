9eb57c1a6f082f8c5bf26caa2b19d9bed0412428
Kuvalesh <121540581+Kuvalesh@users.noreply.github.com>
2023-09-19 15:15:28 +0530

    Update dijkstra's algo.c

--- dijkstra's algo.c
+++ dijkstra's algo.c
@@ -98,3 +98,20 @@ Distance of node2=4
 Path=2<-1
 Distance of node3=8
 Path=3<-2<-1
+
+
+ function dijkstra(G, S)
+    for each vertex V in G
+        distance[V] <- infinite
+        previous[V] <- NULL
+        If V != S, add V to Priority Queue Q
+    distance[S] <- 0
+	
+    while Q IS NOT EMPTY
+        U <- Extract MIN from Q
+        for each unvisited neighbour V of U
+            tempDistance <- distance[U] + edge_weight(U, V)
+            if tempDistance < distance[V]
+                distance[V] <- tempDistance
+                previous[V] <- U
+    return distance[], previous[]
